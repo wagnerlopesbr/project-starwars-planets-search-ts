ba2609fadce5d10a4262c6c581e3669e25b8783d
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-10-15 12:36:45 -0300

    WL - Project StarWars Planets Search 6 e 7/9 v1

--- src/componets/Filter.tsx
+++ src/componets/Filter.tsx
@@ -3,9 +3,20 @@ import { PlanetsContext } from '../context/Provider';
 import useFilter from '../hooks/useFilter';
 import { ColumnOptionsType, ComparisonOptionsType } from '../types';
 
+const columnFields = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
+
 function Filter() {
   const { planetsFilter, selectFilter } = useContext(PlanetsContext);
   const [inputFilter, setInputFilter] = useState('');
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+  const [availableColumns, setAvailableColumns] = useState<string[]>(columnFields);
+  const [appliedFilters, setAppliedFilters] = useState<any[]>([]);
 
   const { filter, handleFilter } = useFilter({
     columnFilter: 'population' as ColumnOptionsType,
@@ -13,23 +24,43 @@ function Filter() {
     valueFilter: 0,
   });
 
-  const columnFields = [
-    'population',
-    'orbital_period',
-    'diameter',
-    'rotation_period',
-    'surface_water',
-  ];
-
-  const comparisonFields = [
-    'maior que',
-    'menor que',
-    'igual a',
-  ];
+  const comparisonFields = ['maior que', 'menor que', 'igual a'];
 
   useEffect(() => {
     planetsFilter(inputFilter);
   }, [inputFilter, planetsFilter]);
+
+  const applyFilter = () => {
+    selectFilter({
+      column: filter.columnFilter,
+      comparison: filter.comparisonFilter,
+      value: Number(filter.valueFilter),
+    });
+
+    const selectedFilterText = `
+      Coluna: ${filter.columnFilter} //
+      Operador: ${filter.comparisonFilter} //
+      Valor: ${filter.valueFilter}`;
+    setSelectedFilters([...selectedFilters, selectedFilterText]);
+
+    setAppliedFilters([...appliedFilters, filter]);
+
+    setAvailableColumns(
+      (prevColumns) => prevColumns.filter((col) => col !== filter.columnFilter),
+    );
+  };
+
+  const removeFilter = (index: any) => {
+    const removedFilter = appliedFilters.splice(index, 1)[0];
+    setAvailableColumns((prevColumns) => [
+      ...prevColumns,
+      removedFilter.column,
+    ]);
+
+    selectedFilters.splice(index, 1);
+    setSelectedFilters([...selectedFilters]);
+    setAppliedFilters(appliedFilters);
+  };
 
   return (
     <div>
@@ -39,6 +70,7 @@ function Filter() {
           value={ inputFilter }
           onChange={ ({ target }) => setInputFilter(target.value) }
           data-testid="name-filter"
+          placeholder="teste"
         />
       </div>
       <label>
@@ -50,8 +82,10 @@ function Filter() {
           value={ filter.columnFilter }
           onChange={ handleFilter }
         >
-          { columnFields.map((column) => (
-            <option key={ column } value={ column }>{column}</option>
+          {availableColumns.map((column) => (
+            <option key={ column } value={ column }>
+              {column}
+            </option>
           ))}
         </select>
       </label>
@@ -64,8 +98,10 @@ function Filter() {
           value={ filter.comparisonFilter }
           onChange={ handleFilter }
         >
-          { comparisonFields.map((comparison) => (
-            <option key={ comparison } value={ comparison }>{comparison}</option>
+          {comparisonFields.map((comparison) => (
+            <option key={ comparison } value={ comparison }>
+              {comparison}
+            </option>
           ))}
         </select>
       </label>
@@ -76,16 +112,18 @@ function Filter() {
         value={ filter.valueFilter }
         onChange={ handleFilter }
       />
-      <button
-        data-testid="button-filter"
-        onClick={ () => selectFilter({
-          column: filter.columnFilter,
-          comparison: filter.comparisonFilter,
-          value: Number(filter.valueFilter),
-        }) }
-      >
+      <button data-testid="button-filter" onClick={ applyFilter }>
         Filtrar
       </button>
+
+      <div>
+        {selectedFilters.map((selectedFilter, index) => (
+          <div key={ index } data-testid="filter">
+            {selectedFilter}
+            <button onClick={ () => removeFilter(index) }>X</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
