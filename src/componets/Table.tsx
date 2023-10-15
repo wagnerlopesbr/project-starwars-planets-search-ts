06085e897f80409718fec45474df134ad1e4fd86
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-10-15 17:01:15 -0300

    WL - Project StarWars Planets Search 9/9 vTESTE 6

--- src/componets/Table.tsx
+++ src/componets/Table.tsx
@@ -73,11 +73,19 @@ function Table() {
             {filterPlanets
               && filterPlanets.map((planet: PlanetsType) => (
                 <tr key={ planet.name }>
-                  {headerKeys.map((headerKey) => (
-                    <td key={ headerKey } data-testid="planet-name">
-                      {planet[headerKey as keyof PlanetsType]}
-                    </td>
-                  ))}
+                  <th data-testid="planet-name">{planet.name}</th>
+                  <th>{planet.rotation_period}</th>
+                  <th>{planet.orbital_period}</th>
+                  <th>{planet.diameter}</th>
+                  <th>{planet.climate}</th>
+                  <th>{planet.gravity}</th>
+                  <th>{planet.terrain}</th>
+                  <th>{planet.surface_water}</th>
+                  <th>{planet.population}</th>
+                  <th>{planet.films}</th>
+                  <th>{planet.created}</th>
+                  <th>{planet.edited}</th>
+                  <th>{planet.url}</th>
                 </tr>
               ))}
           </tbody>
